a789cf79de9296b553213ecb1ef4320edd48cd9e
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-19 14:58:28 +0700

    create looping using while

---
+++ Pemdas2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    while (bilangan%2){
+        cout << "bilang" << bilangan << "adalah ganjil" << endl;
+        bilangan = rand() % 10;
+    }
+    cout << "bilangan" << bilangan << "adalah genap" << endl;
+
+}
